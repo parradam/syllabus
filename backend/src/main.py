87387d970be81082d5a10c38b59697fac03ac007
Adam Parr <adamparr.uk@gmail.com>
2026-02-24 21:56:55 +0000

    chore: get frontend and backing talking

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/health")
-def get_health() -> Response:
-    return Response("ok")
+def get_health() -> dict[str, str]:
+    return {"status": "ok"}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [health, setHealth] = useState<string | null>(null)
+
+  useEffect(() => {
+    const getHealthCheck = async () => {
+      const response = await fetch("http://localhost:8000/health")
+      const data = await response.json()
+      setHealth(data.status)
+    }
+    
+    getHealthCheck();
+  }, [])
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Response: {health}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
